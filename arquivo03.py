289b18fb16abd9b1abe73a9eea43f1e006e2d114
Henrique <henriqueaguiar670@gmail.com>
2025-08-15 00:15:20 +0000

    update

---
+++ arquivo03.py
@@ -0,0 +1,11 @@
+class Jogador:
+    def __init__(self,nome,pontos):
+        self.nome = nome
+        self.pontos = 0
+
+    def adicionar_pontos(self, valor):
+        self.pontos += valor
+
+    def exibir_pontos(self):
+        print(f"Jogador: {self.nome} | Pontos: {self.pontos}")
+        
---
+++ arquivo04.py
@@ -0,0 +1,5 @@
+from arquivo03 import Jogador
+
+j1 = Jogador("Tiago", 0)
+j1.adicionar_pontos(10)
+j1.exibir_pontos()
