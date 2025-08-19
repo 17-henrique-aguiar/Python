66d4e062ae24e5cc61cfb92164bc3d50f9ceb266
Henrique <henriqueaguiar670@gmail.com>
2025-08-19 20:32:05 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+class ContaBancaria:
+    def __init__(self,titular,saldo=0):
+        self.titular = titular
+        self.__saldo = saldo
+
+    # Método para depositar
+    def depositar(self, valor):
+        if valor > 0:
+            self.__saldo += valor
+            return True
+        return False
+    
+    # Método para saque
+    def sacar(self, valor):
+        if 0 < valor <= self.__saldo:
+            self.__saldo -= valor
+            return True
+        return False
+    
+    # Getter para saldo
+    def get_saldo(self):
+        return self.__saldo
