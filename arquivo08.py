0b5cb6fc29d18b22907278d94f9c0d6397eb49e2
Henrique <henriqueaguiar670@gmail.com>
2025-08-19 19:25:46 -0300

    Update arquivo08.py

--- arquivo08.py
+++ arquivo08.py
@@ -2,11 +2,11 @@ from arquivo07 import ContaBancaria
 
 conta1 = ContaBancaria("Renato", 2000.00)
 
-print(f"Titular: {conta1.get_titu()} | Saldo: {conta1.get_saldo():.2f}")
+print(f"Titular: {conta1.get_titular()} | Saldo: {conta1.get_saldo():.2f}")
 
-conta1.set_titu("Renato Silva")
+conta1.set_titular("Renato Silva")
 conta1.set_saldo(2500.00)
-print(f"Titular:{conta1.get_titu()} | Saldo:{conta1.get_saldo():.2f}")
+print(f"Titular:{conta1.get_titular()} | Saldo:{conta1.get_saldo():.2f}")
 
-conta1.set_titu("")
-conta1.se1_saldo(-180)
+conta1.set_titular("")
+conta1.set_saldo(-180)
