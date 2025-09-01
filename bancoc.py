bde8a149927f56f262b50a1a87e085c7908620cf
Henrique <henriqueaguiar670@gmail.com>
2025-09-01 20:39:00 -0300

    Add files via upload

---
+++ bancoc.py
@@ -0,0 +1,19 @@
+class ContaBancaria:
+    def __init__(self, titular, saldo=0):
+        self.titular = titular
+        self.__saldo = saldo
+
+    def depositar(self, valor):
+        if valor > 0:
+            self.__saldo += valor
+            return True
+        return False
+    
+    def sacar(self, valor):
+        if 0 < valor <= self.__saldo:
+            self.__saldo -= valor
+            return True
+        return False
+    
+    def get_saldo(self):
+        return self.__saldo
