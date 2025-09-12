44f65485e659aff8ed0ea63de1da16c12225f0dc
Henrique <henriqueaguiar670@gmail.com>
2025-09-12 20:17:26 -0300

    Add files via upload

--- mercado/__init__.py
+++ mercado/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mercado.db"
+app.config["SECRET_KEY"] = '010d80d71cd1b357d38a9e20'
 db.init_app(app)
 
 from mercado import routes
-
--- mercado/routes.py
+++ mercado/routes.py
@@ -1,10 +1,16 @@
 from mercado import app
 from flask import render_template
 from mercado.models import Item
+from mercado.forms import CadastroForm
 
 @app.route('/')
 def page_home():
     return render_template("home.html")
+
+@app.route('/cadastro')
+def page_cadastro():
+    form = CadastroForm()
+    return render_template("cadastro.html",form=form)
 
 @app.route('/produtos')
 def page_produto():
