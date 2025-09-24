508c49af8d3bf25e63cde15313d70bc01a3f374b
Henrique <henriqueaguiar670@gmail.com>
2025-09-24 19:21:10 -0300

    Add files via upload

--- mercado/__init__.py
+++ mercado/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template  
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mercado.db"
 app.config["SECRET_KEY"] = '010d80d71cd1b357d38a9e20'
 db.init_app(app)
+bcrypt=Bcrypt(app)
 
 from mercado import routes
